391e29ea2cf56f02ba2cde90e2fe7357d5faadc2
Katherine <89894177+katherinearenas@users.noreply.github.com>
2024-08-27 17:16:08 -0500

    product tags

--- models/index.js
+++ models/index.js
@@ -6,20 +6,29 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-
+  foreignKey: 'category_id'
 });
 
 // Categories have many Products
 Category.hasMany(Product,{
-
+  foreignKey: 'category_id'
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag,{
-
+  through:{
+    model: ProductTag,
+    unique : false
+  },
+  as: 'tagged_products'
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tags'
 
 });
 
